1446a04debded638a20ecf2f64b457bb39e315e1
Kumar Vivek <kv2000in@mac.kv>
2020-10-31 00:51:16 -0400

    UI: trim, mid steering, servos XYZ sliders, motors A and B, working . Need to beautify. Atmega code: modified to readVCC from internal reference as Analogread was off.

--- init/init.ino
+++ init/init.ino
@@ -523,6 +523,31 @@ void sendcounter0value(char doizero)
   Serial.print(":OK>");
   }
 
+long readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+ 
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+ 
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+ 
+  long result = (high<<8) | low;
+ 
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
 //Analog read battery pin voltage using ADC, returns battery voltage in mVs.
 int readbatteryvoltage(char isWSrequestingthis){
   //Full scale 3-18V = ADC 0-1024
@@ -533,8 +558,9 @@ int readbatteryvoltage(char isWSrequestingthis){
   float batteryvoltage;
   int adcreadvalue;
   float adcvoltage;
+  long vcc = readVcc();
   adcreadvalue =analogRead(pinBATTERY);
-  adcvoltage= (adcreadvalue/1024.0)*4.91; //Measured by multimeter vcc
+  adcvoltage= (adcreadvalue/1024.0)*(vcc/1000); 
   batteryvoltage=(adcvoltage+(adcvoltage*(2.2)))*1000;
   if (isWSrequestingthis=='V'){
     Serial.print("<v-");
